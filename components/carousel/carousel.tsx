64407c96f32058781dabb2c0ecdfe36cd6f3d277
Felipe de Goes <felipedegoes@Mac-mini-de-Felipe.local>
2023-08-09 22:06:58 -0300

    fix

--- components/carousel/carousel.tsx
+++ components/carousel/carousel.tsx
@@ -6,7 +6,7 @@ import styles from './carousel.module.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Buttoncarousel from '../buttons/ButtonCarousel';
-import { items } from './../../data/items.json';
+import { items } from './items.json';
 
 export default function BootstrapCarousel() {
   const { bootstrap } = items;
