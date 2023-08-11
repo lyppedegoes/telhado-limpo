f0385bfef98a6544efe4ba9f9dbd876057e19d9a
Felipe de Goes <felipedegoes@Mac-mini-de-Felipe.local>
2023-08-10 21:33:05 -0300

    v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 
 import BootstrapCarousel from './../components/carousel/carousel';
+import Accordion from '@/components/accordion/Accordion';
 import ButtonWhatsApp from '@/components/buttons/ButtonWatsapp';
 
 import styles from './page.module.scss';
+
 import 'normalize.css';
 
 export default function Home() {
@@ -56,6 +58,14 @@ export default function Home() {
             priority
           />
           <h2>ELIMINA OS FUNGOS EXISTENTES</h2>
+        </div>
+      </div>
+      <Accordion />
+      <div className={styles.atendente}>
+        <Image src="svgs/qrcode.svg" alt="Limpeza Rápida" width={150} height={150} priority />
+        <div className={styles.agende}>
+          <p>ENTRE EM CONTATO AGORA MESMO E SOLICITE UM ORÇAMENTO</p>
+          <ButtonWhatsApp />
         </div>
       </div>
     </main>
---
+++ components/accordion/Accordion.tsx
@@ -0,0 +1,56 @@
+'use client';
+import React, { useState } from 'react';
+
+import 'normalize.css';
+
+import styles from './accordion.module.scss';
+
+import dataCollection from './dataCollection.json';
+
+function Accordion() {
+  const [accordion, setActiveAccordion] = useState(-1);
+
+  function toggleAccordion(index) {
+    if (index === accordion) {
+      setActiveAccordion(-1);
+      return;
+    }
+    setActiveAccordion(index);
+  }
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.limiter}>
+          <div>
+            <span className={styles.accordion__title}>Dúvidas Frequentes</span>
+            <h1 className={styles.accordion__subtitle}>TIRE SUAS DÚVIDAS</h1>
+          </div>
+          <div className={styles.accordion__faq}>
+            {dataCollection.map((item, index) => (
+              <div key={index} onClick={() => toggleAccordion(index)}>
+                <div className={styles.accordion__faq_heading}>
+                  <h3 className={accordion === index ? styles.active : ''}>{item.question}</h3>
+                  <div>
+                    {accordion === index ? (
+                      <span className={styles.verticle}>-</span>
+                    ) : (
+                      <span className={styles.horizental}>+</span>
+                    )}
+                  </div>
+                </div>
+                <div>
+                  <p className={accordion === index ? styles.active : styles.inactive}>
+                    {item.answer}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Accordion;
